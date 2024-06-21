f7f097768b057e80ef71bf19261263368a3cc910
이름 <barwait@naver.com>
2024-06-21 22:56:19 +0900

    1

--- mysql/sqls/initialize.sql
+++ mysql/sqls/initialize.sql
@@ -1,12 +1,2 @@
-DROP DATABASE IF EXISTS myapp;
+DROP DATABASE myapp;
 
-CREATE DATABASE myapp;
-USE myapp;
-
-CREATE TABLE lists (
-    id INTEGER AUTO_INCREMENT,
-    value TEXT,
-    PRIMARY KEY (id)
-);
-
-desc lists;
