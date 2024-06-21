3627c6b6f45abff5e7cdda64bd407220445a454c
이름 <barwait@naver.com>
2024-06-21 20:28:48 +0900

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
